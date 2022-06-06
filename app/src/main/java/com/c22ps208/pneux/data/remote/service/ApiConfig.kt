8f2e2da187ab68fcf818a7abaf4c9dd0cda28aed
Raxel AK <raxeladam1@gmail.com>
2022-06-07 01:43:22 +0700

    add api config

---
+++ app/src/main/java/com/c22ps208/pneux/data/remote/service/ApiConfig.kt
@@ -0,0 +1,28 @@
+package com.c22ps208.pneux.data.remote.service
+
+import com.c22ps208.pneux.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApiConfig {
+    companion object {
+        // News API
+        fun getNewsApiService(): NewsApiService {
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BuildConfig.NEWS_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(NewsApiService::class.java)
+        }
+
+        // Other APIs here
+    }
+}
---
+++ app/src/main/java/com/c22ps208/pneux/data/remote/service/NewsApiService.kt
@@ -0,0 +1,16 @@
+package com.c22ps208.pneux.data.remote.service
+
+import com.c22ps208.pneux.BuildConfig
+import com.c22ps208.pneux.data.remote.response.NewsResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface NewsApiService {
+    @GET("top-headlines?country=id&category=health&apiKey=$API_TOKEN")
+    @Headers("Authorization: token $API_TOKEN", "UserResponse-Agent: request")
+    suspend fun getListNews() : ArrayList<NewsResponse>
+
+    companion object {
+        private const val API_TOKEN = BuildConfig.NEWSAPI_TOKEN
+    }
+}
